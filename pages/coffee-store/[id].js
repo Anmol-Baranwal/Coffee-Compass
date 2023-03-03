7081c62e4e2ae8ed57b19b8c59610acb0ab522cf
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-03-03 17:34:10 +0530

    add initital dynamic styling with structure

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,12 +1,100 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+import cls from "classnames";
 
-const coffeeStore = () => {
+import coffeeStoreData from "../../data/coffee-stores.json";
+
+import styles from "../../styles/coffee-store.module.css";
+
+export function getStaticProps(staticProps) {
+  const params = staticProps.params; // we can also destructure params in above parameter directly
+  return {
+    props: {
+      coffeeStore: coffeeStoreData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id; // dynamic id
+      }),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  const paths = coffeeStoreData.map((coffeeStore) => {
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
+  return {
+    // paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    paths,
+    fallback: true,
+  };
+}
+
+const coffeeStore = (props) => {
   const router = useRouter();
-  return <div>Coffee Page nested routing {router.query.id}</div>;
-  <Link href="/">
-    <a>Go to home page</a>
-  </Link>; // it doesnt refresh the page like <a> tag
+
+  if (router.isFallback) {
+    return <div>Loading State</div>;
+  }
+
+  const { address, name, neighbourhood, imgURL } = props.coffeeStore;
+
+  const handleUpvoteButton= () => {
+    console.log("what");
+  };
+
+  // <div>Coffee Page nested routing {router.query.id}</div>
+  // <Link href="/coffee-store/dynamic">
+  // <>Go to dynamic page</>
+  // </Link>{" "}
+  // it doesnt refresh the page like anchor tag
+  return (
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <>Go to home page</>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            // src={imgURL}
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          ></Image>
+        </div>
+        <div className={cls("glass", styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/places.svg" width="24" height="24" />
+            <p className={styles.text}>{address}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/near.svg" width="24" height="24" />
+            <p className={styles.text}>{neighbourhood}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/star.svg" width="24" height="24" />
+            <p className={styles.text}>1</p>
+          </div>
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+            Up vote!
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default coffeeStore;
