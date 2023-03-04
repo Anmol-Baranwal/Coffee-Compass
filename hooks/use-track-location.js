fa2fbb6f3fba2c58319f9d1dc144145b35e22f3d
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-03-05 03:33:23 +0530

    add dispatch & usecontext

--- hooks/use-track-location.js
+++ hooks/use-track-location.js
@@ -1,17 +1,25 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
+
+import {ACTION_TYPES, storeContext} from "../pages/_app"
 
 const useTrackLocation = () => {
     const [locationErrorMsg, setLocationErrorMsg] = useState("");
 
-    const [latLong, setLatLong] = useState("");
+    // const [latLong, setLatLong] = useState("");
 
     const [isFindingLocation, setFindingLocation] = useState(false);
+
+    const {dispatch} = useContext(storeContext)
 
     const success = (position) => {
         const latitude= position.coords.latitude;
         const longitude= position.coords.longitude;
 
-        setLatLong(`${latitude}, ${longitude}`)
+        // setLatLong(`${latitude}, ${longitude}`)
+        dispatch({
+            type: ACTION_TYPES.SET_LAT_LONG,
+            payload: {latLong: `${latitude},${longitude}`}
+        })
         setLocationErrorMsg("");
         setFindingLocation(false);
     }
@@ -33,7 +41,7 @@ const useTrackLocation = () => {
     }
 
     return {
-        latLong,
+        // latLong,
         handleTrackLocation,
         locationErrorMsg,
         isFindingLocation,
