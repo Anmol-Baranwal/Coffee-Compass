f7292fcaf734b66339a668aa222a7e5c8a763968
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-03-05 03:05:32 +0530

    use create context

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { createContext } from "react";
+
+const StoreContext = createContext();
+
+const StoreProvider = ({ children }) => {
+  const initialState = {
+    latLong: "",
+    coffeeStores: [],
+  };
+  return (
+    <StoreContext.Provider value={{ state: initialState }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return;
+  <StoreProvider>
+    <Component {...pageProps} />
+  </StoreProvider>;
 }
