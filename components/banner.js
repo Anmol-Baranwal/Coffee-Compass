c8e7fa5fe6c889bba0a81d93c274d5292626bc4c
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-03-22 01:00:58 +0530

    add banner with styling

--- components/banner.js
+++ components/banner.js
@@ -4,7 +4,7 @@ const Banner = (props) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <span className={styles.title1}>The coffee</span>
+        <span className={styles.title1}>The coffee </span>
         <span className={styles.title2}>Project</span>
       </h1>
       <p className={styles.subTitle}>View coffee stores nearby</p>
